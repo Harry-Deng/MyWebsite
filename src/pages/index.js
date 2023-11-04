7e9bcf547eafed087eaf44f863dceb05027869f6
Harry <2307646552@qq.com>
2023-11-04 14:16:21 +0800

    update bagde

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,40 +53,27 @@ export default function Index() {
       <div className={`container padding-bottom--lg ${styles.badges}`}>
         <h2 className="margin-top--lg">技能</h2>        
         <div>
-          <img alt="HTML5" src="https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue"/>
-          <img alt="CSS3" src="/img/badge/CSS3.svg"/>
-          <img alt="JavaScript" src="/img/badge/JAVASCRIPT.svg"/>
-          <img alt="Vue.js" src="/img/badge/VUE.svg"/>
-          <img alt="Node.js" src="/img/badge/NODE.svg"/>
-          <img alt="Swift" src="/img/badge/SWIFT.svg"/>
-          <img alt="LaTeX" src="/img/badge/LATEX.svg"/>
+          <img alt="Python" src="/img/badge/Python.svg"/>
+          <img alt="Pytorch" src="/img/badge/Pytorch.svg"/>
+          <img alt="TensorFlow" src="/img/badge/TensorFlow.svg"/>
+          <br/>
+          <img alt="JAVA" src="/img/badge/JAVA.svg"/>
+          <img alt="JavaFX" src="/img/badge/JavaFX.svg"/>
+          <img alt="Android" src="/img/badge/Android.svg"/>
+          <br/>
+          <img alt="C++" src="/img/badge/C++.svg"/>
+          <img alt="Qt" src="/img/badge/Qt.svg"/>
+          <img alt="C" src="/img/badge/C.svg"/>
+          <img alt="Arduino" src="/img/badge/Arduino.svg"/>
+          <img alt="LaTeX" src="/img/badge/LaTeX.svg"/>
         </div>
         
         <h2 className="margin-top--lg">工具</h2>
         <div>
-          <img alt="VS Code" src="/img/badge/VS CODE.svg"/>
-          <img alt="Vercel" src="/img/badge/VERCEL.svg"/>
-          <img alt="Git" src="/img/badge/GIT.svg"/>
-          <img alt="Docker" src="/img/badge/DOCKER.svg"/>
-          <img alt="Quasar" src="/img/badge/QUASAR.svg"/>
-          <img alt="TailwindCSS" src="/img/badge/TAILWIND CSS.svg"/>
-          <img alt="Overleaf" src="/img/badge/OVERLEAF.svg"/>
-          <img alt="Xcode" src="/img/badge/XCODE.svg"/>
-          <img alt="Socket.io" src="/img/badge/SOCKET.IO.svg"/>
-          <img alt="Axios" src="/img/badge/AXIOS.svg"/>
-          <img alt="Font Awesome" src="/img/badge/FONT AWESOME.svg"/>
-          <img alt="Sketch" src="/img/badge/SKETCH.svg"/>
-          <img alt="Postman" src="/img/badge/POSTMAN.svg"/>
-          <img alt="Sass" src="/img/badge/SASS.svg"/>
+          <img alt="Microsoft_Office" src="/img/badge/Microsoft_Office.svg"/>
+          <img alt="Overleaf" src="/img/badge/Overleaf.svg"/>
+          <img alt="VSCode" src="/img/badge/VSCode.svg"/>
         </div>
-
-        <h2 className="margin-top--lg">学习</h2>
-        <div>
-          <img alt="PostCSS" src="/img/badge/POSTCSS.svg"/>
-          <img alt="UnoCSS" src="/img/badge/UNOCSS.svg"/>
-          <img alt="Python" src="/img/badge/PYTHON.svg"/>
-        </div>
-
 
       </div>
     </Layout>
